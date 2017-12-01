b9e15d9b56984a1a898876d44eb52d6ba9d2ac61
Omar Ezzat <omar.ezzat@protonmail.com>
2017-12-01 23:36:30 +0000

    Included libs, models, images. Added rudimentary detect and classify functions. (not working)

--- assets/js/util.js
+++ assets/js/util.js
@@ -47,10 +47,13 @@ function drawImgTo(canvasId, image){
       if(!image){
         reject('no image provided');
       } 
-      console.log('draw1 to ' + canvasId);
-      console.log(image);
-      var ctx = document.getElementById(canvasId).getContext('2d');
+      // console.log('draw1 to ' + canvasId);
+      // console.log(image);
       var resizeFactor = (200 / image.width);
+      var canvas = document.getElementById(canvasId);
+      canvas.width = image.width * resizeFactor;
+      canvas.height = image.height * resizeFactor;
+      var ctx = canvas.getContext('2d');
       ctx.width = image.width * resizeFactor;
       ctx.height = image.height * resizeFactor;
       ctx.drawImage(image, 0, 0, ctx.width, ctx.height);
@@ -88,27 +91,40 @@ function darken(image){
 
 function detectFace(image){
   return new Promise(function(resolve, reject){
-    var canvas = document.getElementById('canvas');
+    var canvas = document.createElement('canvas');
     canvas.height = image.height;
     canvas.width = image.width;
     var context = canvas.getContext('2d');
     context.height = image.height;
     context.width = image.width;
     context.drawImage(image, 0, 0, context.width, context.height);
-    var classifier = objectdetect.frontalface;
-    // classifier = objectdetect[e.target.value];
+    // var classifier = objectdetect.frontalface;
+    var classifier = objectdetect.frontalface_alt;
     detector = new objectdetect.detector(canvas.width, canvas.height, 1.2, classifier);
-    var rects = detector.detect(canvas, 1);
-    
-    // Draw rectangles around detected faces:
+    var rects = detector.detect(canvas, 1, 1);
+    // find the greates rect
+    var greatest = 0;
+    var maxRect = 0;
     for (var i = 0; i < rects.length; ++i) {
       var coord = rects[i];
-      context.beginPath();
-      context.lineWidth = 1;
-      context.strokeStyle = 'rgba(0, 255, 255, 0.75)';
-      context.rect(coord[0], coord[1], coord[2], coord[3]);
-      context.stroke();
+      var area = coord[2] * coord[3];
+      if ( area > maxRect){
+        maxRect = area;
+        greatest = i;
+      }
     }
+    // console.log(rects[greatest][2],rects[greatest][3])
+    canvas.width = context.width = rects[greatest][2];
+    canvas.height = context.height = rects[greatest][3];
+    context.drawImage(image, rects[greatest][0], rects[greatest][1], rects[greatest][2], rects[greatest][3],
+                             0, 0, rects[greatest][2], rects[greatest][3])
+    var img = new Image(context.width, context.height);
+    img.onload = function() {
+        console.log('detectFace: ', img.width, img.height);
+        resolve(img);
+    }
+    img.src = canvas.toDataURL('image/jpeg');
+ 
   });
 }
 
@@ -132,6 +148,64 @@ function resize(image, newWidth, newHeigth){
   });
 }
 
+function classify(image){
+  // TODO: try new model
+  const model = new KerasJS.Model({
+    filepaths: {
+      model: '../../../../../assets/keras-models/model4.json',
+      weights: '../../../../../assets/keras-models/model4_weights.buf',
+      metadata: '../../../../../assets/keras-models/model4_metadata.json'
+      // model: '../../../../../assets/keras-models/resnet50.json',
+      // weights: '../../../../../assets/keras-models/resnet50_weights.buf',
+      // metadata: '../../../../../assets/keras-models/resnet50_metadata.json'
+    },
+    gpu: true
+  });
+
+  var canvas = document.createElement("canvas");
+  canvas.height = image.height;
+  canvas.width = image.width;
+  var ctx = canvas.getContext("2d");
+  ctx.height = image.height;
+  ctx.width = image.width;
+  ctx.drawImage(image,0,0);
+  var imgd = ctx.getImageData(0, 0, ctx.width, ctx.height);
+  var pix = imgd.data;
+  var floatArr = [];
+  for(var i = 0; i<pix.length; i+=4){
+  var greyValue = (pix[i] + pix[i+1] + pix[i+2])/(3 * 255);
+    floatArr.push(greyValue);
+  }
+  console.log(imgd, floatArr.length, floatArr);
+  console.log(model);
+  return model.ready()
+  .then(() => {
+    // input data object keyed by names of the input layers
+    // or `input` for Sequential models
+    // values are the flattened Float32Array data
+    // (input tensor shapes are specified in the model config)
+    const inputData = {
+      'input': new Float32Array(floatArr)
+      // 'conv2d_8': new Float32Array(floatArr)
+    }
+
+    // make predictions
+    return model.predict(inputData)
+  })
+  .then(outputData => {
+    // outputData is an object keyed by names of the output layers
+    // or `output` for Sequential models
+    // e.g.,
+    // outputData['fc1000']
+    console.log(outputData);
+  })
+  .catch(err => {
+    // handle error
+    console.log('ERROR:',err);
+  });
+}
+
+
 function loadDefaultImage(e){
   // get image
   getLocalImage(e)
@@ -144,6 +218,12 @@ function loadDefaultImage(e){
     console.log(image.width, image.height);
     // return darken(image);
     return resize(image, 48, 48);
+    // return image;
+  })
+  .then(function(image){
+    classify(image).then(function(label){
+    });
+    return image;
   })
   .then(function(image){
     // console.log(img[0].src);
